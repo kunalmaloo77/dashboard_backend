5fed531737384d5e35b9677a7f34ea4a7eb5c87d
Kunal Maloo <kunnalmaloo@gmail.com>
2024-03-08 22:19:12 +0530

    fix: now able to filter clients based on confirmed order

--- controller/client.js
+++ controller/client.js
@@ -8,64 +8,85 @@ exports.getAllClients = async (req, res) => {
     }
     catch (error) {
         console.log(error);
-    }  
+    }
 };
 
+//Read clients without address
+exports.getOrders = async (req, res) => {
+    try {
+        const orders = await clientModel.find({ address: { $exists: false } });
+        res.json(orders);
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+//Read client with address
+exports.getConfirmedOrders = async (req, res) => {
+    try {
+        const confirmed = await clientModel.find({ address: { $exists: true } });
+        res.json(confirmed);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 //Read single GET /clients/:id
-exports.getClient = async(req, res) => {
+exports.getClient = async (req, res) => {
     const id = req.params.id;
     const client = await clientModel.findById(id);
     res.json(client);
 };
 
 //Create POST /clients
-exports.createClient = async(req, res) => {
+exports.createClient = async (req, res) => {
     try {
         const clientDoc = new clientModel(req.body);
         console.log(req.body);
         const doc = await clientDoc.save();
         console.log(doc);
         res.status(201).json(doc);
-    } 
+    }
     catch (error) {
         console.error(error);
         res.status(400).json(error);
-    }          
+    }
 };
 
 // Replace PUT /clients/:id
 exports.replaceClient = async (req, res) => {
     const id = req.params.id;
-    try{
-        const doc = await clientModel.findOneAndReplace({_id:id},req.body,{new:true})
+    try {
+        const doc = await clientModel.findOneAndReplace({ _id: id }, req.body, { new: true })
         res.status(201).json(doc);
-        }
-        catch(err){
-          console.log(err);
-          res.status(400).json(err);
-        }
+    }
+    catch (err) {
+        console.log(err);
+        res.status(400).json(err);
+    }
 };
 
 //Update PATCH /clients/:id
-exports.updateClient =  async (req, res) => {
+exports.updateClient = async (req, res) => {
     const id = req.params.id;
-    try{
-    const doc = await clientModel.findOneAndUpdate({_id:id},req.body,{new:true})
-    res.status(201).json(doc);
+    try {
+        const doc = await clientModel.findOneAndUpdate({ _id: id }, req.body, { new: true })
+        res.status(201).json(doc);
     }
-    catch(err){
-      console.log(err);
-      res.status(400).json(err);
+    catch (err) {
+        console.log(err);
+        res.status(400).json(err);
     }
 };
 
 //Delete DELETE /clients/:id
 exports.deleteClient = async (req, res) => {
-    try{
-        const doc = await clientModel.findOneAndDelete({_id:id})
+    try {
+        const doc = await clientModel.findOneAndDelete({ _id: id })
         res.status(201).json(doc);
     }
-    catch(err){
+    catch (err) {
         console.log(err);
         res.status(400).json(err);
     }
--- routes/client.js
+++ routes/client.js
@@ -5,11 +5,13 @@ console.log(clientController);
 const clientRouter = express.Router();
 
 clientRouter
-.post('/', clientController.createClient)
-.get('/', clientController.getAllClients)
-.get('/:id', clientController.getClient)
-.put('/:id', clientController.replaceClient)
-.patch('/:id', clientController.updateClient)
-.delete('/:id', clientController.deleteClient)
+    .post('/', clientController.createClient)
+    .get('/', clientController.getAllClients)
+    .get('/orders', clientController.getOrders)
+    .get('/confirmed', clientController.getConfirmedOrders)
+    .get('/:id', clientController.getClient)
+    .put('/:id', clientController.replaceClient)
+    .patch('/:id', clientController.updateClient)
+    .delete('/:id', clientController.deleteClient)
 
 module.exports = clientRouter;
